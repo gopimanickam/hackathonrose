e94503279ca8eda1a98864fe5cc0a6ac0e527609
Gopi Manickam <gopi.manickam@iamneo.ai>
2023-09-16 22:26:55 +0530

    frontend modify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
-  { path: 'login', component: LoginComponent },
-  { path: 'test', component: PsychometricTestComponent, canActivate: [AuthGuard] },
+ 
 ];
 
 @NgModule({
